d6886d6056ee9fd1ac3166983b70ee17bd4f5329
monibm <monibm@LT5CG4504GC4.indexis.lan>
2016-09-05 14:43:46 +0200

    updated version

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
-copy /Y D:\Users\monibm\git\robotframework-filelibrary\target\filelibrary-0.0.1-SNAPSHOT.jar c:\robot-tests\resources\lib\filelibrary.jar
+copy /Y D:\Users\monibm\git\robotframework-filelibrary\target\filelibrary-1.0.0-SNAPSHOT.jar c:\robot-tests\resources\lib\filelibrary.jar
 python -m robot.libdoc FileLibrary C:\robot-tests\resources\lib\filelibrary.xml
 python -m robot.libdoc FileLibrary C:\robot-tests\documentation\filelibrary.html
