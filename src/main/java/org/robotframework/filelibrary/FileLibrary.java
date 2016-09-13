e903185bb959d0ec6ab9153648f53e607eaadabc
monibm <monibm@LT5CG4504GC4.indexis.lan>
2016-09-13 16:01:32 +0200

    updated doc

--- src/main/java/org/robotframework/filelibrary/FileLibrary.java
+++ src/main/java/org/robotframework/filelibrary/FileLibrary.java
@@ -8,11 +8,15 @@ public class FileLibrary implements KeywordDocumentationRepository, RobotJavaLib
 
 	public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
 
-	public static final String LIBRARY_DOCUMENTATION = "FileLibrary is a Robot Framework test library for generating test data files.\n"
-			+ "Built in variables: now";
-
-	private final AnnotationLibrary annotationLibrary = new AnnotationLibrary(
-			"org/robotframework/filelibrary/keyword/*.class");
+	// @formatter:off
+	public static final String LIBRARY_DOCUMENTATION = "FileLibrary is a Robot Framework test library for generating test data files.\n\n"
+			+ "Built in variables: ${now}, ${today}\n\n"
+			+ "For SQL related commands, you can specify SQL satements either directly in the argument or provide them in an .sql file. \n\n\n"
+			+ " | *IMPORTANT* | For statements included directly in the test case, you need to escape any template data variables. If you do not, it will not be able to find the value. | \n"
+			+ " | _Example_ | Execute SQL	update mytable set value = 2 where id = \\${my_id} | \n";
+	
+	// @formatter:on
+	private final AnnotationLibrary annotationLibrary = new AnnotationLibrary("org/robotframework/filelibrary/keyword/*.class");
 
 	@Override
 	public Object runKeyword(String keywordName, Object[] args) {
