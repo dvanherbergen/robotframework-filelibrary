d60554f91459fc25d2324c7d6e77028c828432a5
monibm <monibm@LT5CG4504GC4.indexis.lan>
2016-09-15 10:06:00 +0200

    Added UUID

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
-copy /Y D:\Users\monibm\git\robotframework-filelibrary\target\filelibrary-1.0.0-SNAPSHOT.jar c:\robot-tests\resources\lib\filelibrary.jar
-copy /Y D:\Users\monibm\git\robotframework-filelibrary\doc\filelibrary.xml c:\robot-tests\resources\lib\
-copy /Y D:\Users\monibm\git\robotframework-filelibrary\doc\filelibrary.html c:\robot-tests\documentation\
+copy /Y D:\Users\%USERNAME%\git\robotframework-filelibrary\target\filelibrary-1.0.0-SNAPSHOT.jar c:\robot-tests\resources\lib\filelibrary.jar
+copy /Y D:\Users\%USERNAME%\git\robotframework-filelibrary\doc\filelibrary.xml c:\robot-tests\resources\lib\
+copy /Y D:\Users\%USERNAME%\git\robotframework-filelibrary\doc\filelibrary.html c:\robot-tests\documentation\
